1f25fc0ddf2451acfa9736f63293cd732b10c9d4
Personal <personal@ITSupports-MacBook-Pro.local>
2022-03-19 23:05:48 +0530

    Resolved some unused statements

--- src/main/java/com/mongo/project1/repository/UserRepository.java
+++ src/main/java/com/mongo/project1/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.mongo.project1.repository;
 
 import com.mongo.project1.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 public interface UserRepository extends MongoRepository<User, String> {
     User findUserByName(String name);
